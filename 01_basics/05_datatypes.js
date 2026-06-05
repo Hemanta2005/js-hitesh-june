9d651f58c152fa18dfddf915857c3d867dd5bbbc
Hemanta2005 <hemantabiswas2005@gmail.com>
2026-06-05 20:10:17 +0000

    Summaries of DataTypes

--- 01_basics/05_datatypes.js
+++ 01_basics/05_datatypes.js
@@ -50,5 +50,35 @@ console.log(typeof myFunction); //Function (it is object function)
 
 
 
+//++++++++++++++++++++
+
+
+//Stack(Primitive), Heap(Non-Primitive)  
+let myName = "Hemanta"
+let anotherName = myName
+anotherName = "Rick"
+console.log(anotherName);
+console.log(myName);
+//Here in stack (primitive) we create coppy and we change the values inside coppy so the value inside the original never changes 
+
+
+let user1 = {
+    email: "hjfer@gmail.com",
+    upi: "efkjb@upi" 
+}
+
+
+let user2 = user1
+
+user2.email = "nfeuih.hil"
+
+console.log(user1.email);
+console.log(user2.email);
+
+//Here in Heap(Non-Primitive) we change the values directly in the original we don't create any coppies here 
+
+
+
+
 
 
