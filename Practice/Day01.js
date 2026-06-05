23c17ba9fb921bfb2256c236ed18b7794e52211b
Hemanta2005 <hemantabiswas2005@gmail.com>
2026-06-05 17:24:18 +0000

    Practice of JS

---
+++ Practice/Day01.js
@@ -0,0 +1,26 @@
+const studentId = "Arup"
+let studentAge = 17
+let studentNumber = 672617381
+let studentCity = "Siliguri"
+
+console.table([studentId, studentAge, studentNumber, studentCity]);
+
+
+let score = 1
+console.log(typeof score);
+score = undefined 
+console.log(typeof score); 
+console.log(score);
+
+
+let num1 = 28
+let num2 = 97
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
+console.log(num1%num2);
+
+
+
+
