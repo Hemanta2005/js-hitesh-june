a799921ca9c620499f1e5f87ae02c6630aac467d
Hemanta2005 <hemantabiswas2005@gmail.com>
2026-06-06 01:36:06 +0000

    Strings

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,41 @@
+const name  ="Hemanta"
+const repoCount = 67
+
+//console.log(name + repoCount+ " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Hemanta-HC-com')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+
+const newString = gameName.substring(0, 4) 
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)  //revrse mein ayega 
+console.log(anotherString);
+
+
+const newString1 = "    heamnta     "
+console.log(newString1);
+console.log(newString1.trim());  //method to remove whitespaces
+
+
+
+
+const url = "https://erbhj.com/hitura20hijhd"
+console.log(url.replace('20', '-'));
+
+console.log(url.includes('hennn'));
+
+console.log(gameName.split('-')); 
+
+ 
