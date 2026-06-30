14cc52006a8cc9726b6b3377bf6a5695e1ddf3a2
Hemanta2005 <hemantabiswas2005@gmail.com>
2026-06-30 13:02:54 +0000

    gjcc

---
+++ Practice/func.js
@@ -0,0 +1,35 @@
+function sayHello() {
+    console.log('Hello Hemanta');
+    
+}
+sayHello();
+
+function showBike() {
+    console.log('My fav bikes is Duke 390');
+    
+}
+showBike()
+
+function showAge(){
+    console.log('My age is 20');
+    
+}
+showAge()
+
+function userName(username){
+    if (username === undefined){
+        console.log('Please enter a username!');
+        return
+        
+    }
+    return `Hii ${username}`
+    
+}
+console.log(userName('Hemanta'));
+
+function sum (num1, num2){
+
+    return num1 + num2
+
+}
+console.log();
