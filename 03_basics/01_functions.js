b0bda3125b45050c7329a133c2024fa711fa3a09
Hemanta2005 <hemantabiswas2005@gmail.com>
2026-06-30 12:18:26 +0000

    fhjb

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,51 @@
+/* function addTwoNumbers(number1, number2){
+
+    console.log(number1 + number2)
+}  */
+
+
+
+//Here in this: addTwoNumbers is reference to the function and adding () at the end is calling the function after the reference 
+
+//Here number1 and number2 are PARAMETERS
+//3 and 4 are ARGUEMENTS 
+
+function addTwoNumbers(number1, number2){
+
+    //let result = number1 + number2
+    //return result
+    //console.log('rfrebhj'); This line will not get printed 
+    //cause once u do the return in function 
+    //no other print can be done inside it 
+
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+//console.log('Result: ', result);
+
+
+    
+function loginUserMsg (username){
+    if(username === undefined){
+        console.log("Please enter a username");
+        return
+        
+    }
+    return `${username} just logged in`
+}
+
+console.log(loginUserMsg());
+ //in 34th line if we didn't add anything and put only ''  or  "" the print would be   
+ // just logged in
+
+ //if we didn't even add parenthesis then print wold be 
+ // undefined just logged in 
+
+ //But why????
+ //=>
+
+//How do we repair it??
+//=> We insert an if statement inside our function for the ubndefined case and if there is a undefined then we just want the 
+//function to stop its working and for that we use "return" inside our function that stops the function 
