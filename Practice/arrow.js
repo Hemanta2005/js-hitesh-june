04a6e7f7b9f911a0cd324c43dbe2f09c90b2de88
Hemanta2005 <hemantabiswas2005@gmail.com>
2026-07-04 06:16:28 +0000

    Practice

---
+++ Practice/arrow.js
@@ -0,0 +1,28 @@
+const sayhello = () => {
+    console.log('Hello Hemanta');
+    
+}
+sayhello()
+
+const bike = () => {
+    console.log('KTM Adv 390');
+    
+}
+bike()
+
+const clg = () =>
+{
+    console.log('madarchod college');
+    
+}
+clg()
+
+/*const multiply = (num1, num2) => {
+    return num1*num2
+
+}
+console.log(multiply(9, 7))*/
+
+const multiply1 = (num1, num2) => (num1*num2)
+console.log(multiply1(9, 79));
+
