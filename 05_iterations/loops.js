23f7b9e15de518fe8622dbce984dd668939e80a2
Hemanta2005 <hemantabiswas2005@gmail.com>
2026-07-05 16:05:33 +0000

    For loops practice

--- 05_iterations/loops.js
+++ 05_iterations/loops.js
@@ -1,6 +1,6 @@
 //For 
 
-for (let i = 0; i < '10'; i++) {
+for (let i = 0; i < 10; i++) {
     const element = i;
     if (element == 5){
         //console.log('5 is the best number');
@@ -9,25 +9,25 @@ for (let i = 0; i < '10'; i++) {
     //console.log(element);  
 }
 
-// for (let i = 0; i <= 10 ; i++) {
-//     console.log(`The table of: ${i}`);
+ for (let i = 0; i <= 10 ; i++) {
+     console.log(`The table of: ${i}`);
     
-//     for (let j = 0; j <= 10; j++) {
-//         // console.log(`Inner value: ${j} and outer value: ${i}`);
-//         console.log(i + '*' + j + ' = ' + i*j);
+     for (let j = 0; j <= 10; j++) {
+         // console.log(`Inner value: ${j} and outer value: ${i}`);
+         //console.log(i + '*' + j + ' = ' + i*j);
         
-//     }
+     }
 
-// }
+ }
 
-// let myArr = ["spiderman", "batman", "superman"]
+ let myArr = ["spiderman", "batman", "superman"]
 
-// for (let index = 0; index < myArr.length; index++) {
-//     const element = myArr[index];
-//     console.log(element);
+ for (let index = 0; index < myArr.length; index++) {
+     const element = myArr[index];
+     console.log(element);
     
     
-// }
+ }
 
 //Break and Continue
 
---
+++ Practice/loops.js
@@ -0,0 +1,33 @@
+/*
+const bikes = ["KTM", "BMW", "Yamaha", "Honda"]
+
+for(let i = 0; i < bikes.length; i++){
+    console.log(bikes[i]);
+}
+*/
+
+const bikes = ['yamaha', 'honda', 'ktm', 'suzuki', 'triumph']
+for(let i = 0; i<bikes.length; i++){
+
+    //console.log(bikes[i]);
+    
+}
+
+// const players = ['dhoni', 'bumrah', 'kohli', 'rohit', 'shami']
+// for (let i = 0; i < players.length; i++) {
+//     const element = players[i];
+//     console.log(element); 
+// }
+
+// const teamMembers = ['hemnanta', 'ranajit', 'ripan', 'arya']
+// for (let i = 0; i < teamMembers.length; i++) {
+//     console.log(`Team member ${i+1}: ${teamMembers[i]}`);
+        
+// }
+
+for (let i = 20; i <= 20; i--) {
+    const element = i;
+    console.log(element);
+    
+    
+}
