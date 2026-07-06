b4e6743a5ec0e87afdb375936d96fec569379059
Hemanta2005 <hemantabiswas2005@gmail.com>
2026-07-06 03:16:12 +0000

    loops prac

--- Practice/loops.js
+++ Practice/loops.js
@@ -6,12 +6,12 @@ for(let i = 0; i < bikes.length; i++){
 }
 */
 
-const bikes = ['yamaha', 'honda', 'ktm', 'suzuki', 'triumph']
-for(let i = 0; i<bikes.length; i++){
+// const bikes = ['yamaha', 'honda', 'ktm', 'suzuki', 'triumph']
+// for(let i = 0; i<bikes.length; i++){
 
-    //console.log(bikes[i]);
+//     //console.log(bikes[i]);
     
-}
+// }
 
 // const players = ['dhoni', 'bumrah', 'kohli', 'rohit', 'shami']
 // for (let i = 0; i < players.length; i++) {
@@ -25,9 +25,89 @@ for(let i = 0; i<bikes.length; i++){
         
 // }
 
-for (let i = 20; i <= 20; i--) {
+for (let i = 20; i >= 1; i--) {
     const element = i;
+    //console.log(element);
+    
+    
+}
+
+const scores = [54, 64, 23, 87, 90]
+
+for (let i = 0; i < scores.length; i++) {
+    const element = scores[i];
+    //console.log(`Score is: ${scores[i]}`);
+    
+    
+}
+
+const bikes = ['yamaha', 'suzuki', 'ktm', 'ducati', 'kawasaki']
+for (let brands = 0; brands < bikes.length; brands++) {
+    const element = bikes[brands];
+    //console.log(`Bike ${brands}: ${bikes[brands]}`);
+    
+    
+}
+
+for (let j = 0; j <= 20; j++) {
+    if(j==10){
+        //console.log('Selected number: 10');
+        break;
+    }
+    //console.log(j);
+    
+    
+}
+
+for (let k = 0; k <= 15; k++) {
+    if(k==7){
+        //console.log('skipping 7');
+        continue;
+    }
+    //console.log(k);
+    
+    
+}
+
+const players = [
+    "Virat",
+    "Rohit",
+    "Gill",
+    "Hardik",
+    "Bumrah"
+]
+
+for (let i = 0; i < players.length; i++) {
+    const element = players[i];
+    //console.log(`Player ${i}: ${element}`);
+    
+    
+}
+
+const teams = [
+    "Warriors",
+    "Titans",
+    "Champions",
+    "Kings"
+]
+for (let j = 0; j < teams.length; j++) {
+    const element = teams[j];
+    //console.log(`Team name: ${element}`);
+    
+    
+}
+
+const sports = [
+    "Cricket",
+    "Football",
+    "Badminton",
+    "Basketball"
+]
+for (let index = 0; index < sports.length; index++) {
+    const element = sports[index];
+    console.log(`Sport available:`);
     console.log(element);
     
     
 }
+
